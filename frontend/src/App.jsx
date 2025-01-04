0171bfa6a7de6dfe3a4117e05a9c48fcb4810490
Michael <micaheledquilan@gmail.com>
2025-01-03 19:03:16 -0500

    import drawer plus images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
-
+import Navbar from './Navbar'
+import restaurant_gilder from "./assets/restaurant_gilder.jpg"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      {/* <h1 className="text-3xl font-bold underline">
+        The Restaurant at Gilder
+      </h1> */}
+      <img src={restaurant_gilder} alt="" />
+    </>
+
   )
 }
 
