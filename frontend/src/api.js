45b1a046cc123c2425664f670a7dc898dab76a37
Michael <micaheledquilan@gmail.com>
2025-04-23 20:00:54 -0400

    added frontend api to talk to backend

---
+++ frontend/src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const API_URL = "/api/v1";
+
+//get all menu items -> staff
+export const getMenu = () => axios.get(`${API_URL}/`);
+
+//get all active menu -> customers
+export const getActiveMenu = () => axios.get(`${API_URL}/active`);
+
+//add food item
+export const AddFoodItem = (item) => axios.post(`${API_URL}/`, item);
+
+//update food item
+export const updateFoodItem = (id, item) => axios.put(`${API_URL}/${id}`, item);
+
+//delete food item
+export const deleteFoodItem = (id) => axios.delete(`${API_URL}/${id}`);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   assetsInclude: ["**/*.JPG"],
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": "https://localhost:3000/",
+    },
+  },
 });
