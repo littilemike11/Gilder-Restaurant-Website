3fc08b8aeb3f27061dc2c7b73f69f962abdeeab6
Michael <micaheledquilan@gmail.com>
2025-04-29 14:20:34 -0400

    undo last commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   assetsInclude: ["**/*.JPG"],
-  plugins: [
-    tailwindcss(),
-    react(),
-    viteStaticCopy({
-      targets: [{ src: "public/_redirects", dest: "." }],
-    }),
-  ],
-  base: "/", // Keep this since you're using a custom domain
+  plugins: [tailwindcss(), react()],
   server: {
     proxy: {
       "/api": "http://localhost:3000/",
