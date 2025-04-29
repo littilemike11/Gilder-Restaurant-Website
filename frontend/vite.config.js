05b1548a937815015b0cc627c5fcd1b899f80dec
Michael <micaheledquilan@gmail.com>
2025-04-29 14:14:29 -0400

    added for netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   assetsInclude: ["**/*.JPG"],
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "public/_redirects", dest: "." }],
+    }),
+  ],
+  base: "/", // Keep this since you're using a custom domain
   server: {
     proxy: {
       "/api": "http://localhost:3000/",
