7e2db3c99e1788270fd4b49aa7bf0a0488adde6d
Michael <micaheledquilan@gmail.com>
2025-05-03 15:23:27 -0400

    add proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   base: "/",
   server: {
     proxy: {
-      "/api": "http://localhost:3000/",
+      "/api": process.env.VITE_API_URL || "http://localhost:3000/",
     },
   },
 });
