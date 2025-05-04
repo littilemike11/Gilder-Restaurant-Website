75d25f0c541fad10edfedfdbf77e60436f62d6fc
Michael <micaheledquilan@gmail.com>
2025-05-03 22:51:44 -0400

    chaneg import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./menuRoutes.js";
+import router from "./MenuRoutes.js";
 dotenv.config();
 const app = express();
 
